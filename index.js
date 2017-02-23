c0443f85d87ac99de7249abab7608cb863b57478
Jason Rodriguez <jason.rodriguez@byu.edu>
2017-02-23 10:11:52 -0700

    new data working

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Mongo     = require('mongodb');
 const assert    = require('assert');
 
 var people = 0;
-
+/*
 // Connection URL
 var url = 'mongodb://jrod95:jay-research@ds151108.mlab.com:51108/keystroke-data';
 
@@ -99,15 +99,15 @@ function getDoc(db, str, callback) {
     callback(docs, str);
   });
 }
+*/
 
-/*
 var data = JSON.parse(fs.readFileSync(path.join(__dirname, 'raw', '677125182-data.txt'), 'utf8'));
 var pdf = JSON.parse(fs.readFileSync(path.join(__dirname, 'raw', '677125182-pdf.txt'), 'utf8'));
 
 var parser = new jr_keystroke_analyzer();
 parser.init(pdf, data, '677125182');
 
-*/
+
 
 /******************************************************************
  * Main variable to contail all funcitons out of the global scope *
@@ -136,7 +136,7 @@ function jr_keystroke_analyzer() {
    * @return {void}       void;                                           *
    ************************************************************************/
   this.init = function (pdf, data, str) {
-    
+    /*
     fs.writeFile(path.join(__dirname, 'raw', str + '-data.txt'), JSON.stringify(data), function(err) {
           if (err) {
           return console.log(err);
@@ -151,7 +151,7 @@ function jr_keystroke_analyzer() {
 
         console.log("The file was saved!");
     });
-    
+    */
 
     
     self.createPDF(pdf, str);
